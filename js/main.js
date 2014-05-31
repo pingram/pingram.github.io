0a18d0d03da387f1acc49cae053405fbe518b9e7
Philip Ingram <philip.d.ingram@gmail.com>
2014-05-31 14:32:27 -0700

    got windows working again

---
+++ js/main.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+
+    $('.window').windows({
+        snapping: true,
+        snapSpeed: 500,
+        snapInterval: 400, // originally 1100
+        onScroll: function(scrollPos){
+            // scrollPos:Number
+        },
+        onSnapComplete: function($el){
+            // after window ($el) snaps into place
+        },
+        onWindowEnter: function($el){
+            // when new window ($el) enters viewport
+        }
+    })
+
+});
