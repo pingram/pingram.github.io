930df7371b4fb170c74686ecfb8b1e679b5ef713
Philip Ingram <philip.d.ingram@gmail.com>
2014-06-01 13:36:06 -0700

    add descriptions on portfolio page

--- js/main.js
+++ js/main.js
@@ -1,24 +1,47 @@
 $(document).ready(function(){
 
-    $('.window').windows({
-        snapping: true,
-        snapSpeed: 500,
-        snapInterval: 1100, // originally 1100
-        onScroll: function(scrollPos){
-            // scrollPos:Number
-        },
-        onSnapComplete: function($el){
-            // after window ($el) snaps into place
-        },
-        onWindowEnter: function($el){
-            // when new window ($el) enters viewport
-        }
-    })
+  $('.window').windows({
+      snapping: true,
+      snapSpeed: 500,
+      snapInterval: 1100, // originally 1100
+      onScroll: function(scrollPos){
+          // scrollPos:Number
+      },
+      onSnapComplete: function($el){
+          // after window ($el) snaps into place
+      },
+      onWindowEnter: function($el){
+          // when new window ($el) enters viewport
+      }
+  })
 
-    // $('img').adipoli({
-    //   'startEffect' : 'grayscale',
-    //   'hoverEffect' : 'popout'
-    // });
+  // $('img').adipoli({
+  //   'startEffect' : 'grayscale',
+  //   'hoverEffect' : 'popout'
+  // });
+  
+  $('#project-phlickr').mouseover(function () {
+    $('#phlickr-details').show();
+  })
 
+  $('#project-phlickr').mouseout(function () {
+    $('#phlickr-details').hide();
+  })
+
+  $('#project-trellino').mouseover(function () {
+    $('#trellino-details').show();
+  })
+
+  $('#project-trellino').mouseout(function () {
+    $('#trellino-details').hide();
+  })
+
+  $('#project-asteroids').mouseover(function () {
+    $('#asteroids-details').show();
+  })
+
+  $('#project-asteroids').mouseout(function () {
+    $('#asteroids-details').hide();
+  })
 });
 
