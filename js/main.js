e09655e84ac51b8a122bc33c7214aa9f6f246e10
Philip Ingram <philip.d.ingram@gmail.com>
2015-05-25 19:03:24 -0400

    start using fullpage for window management

--- js/main.js
+++ js/main.js
@@ -1,25 +1,10 @@
 $(document).ready(function(){
 
-  $('.window').windows({
-      snapping: true,
-      snapSpeed: 500,
-      snapInterval: 1100, // originally 1100
-      onScroll: function(scrollPos){
-          // scrollPos:Number
-      },
-      onSnapComplete: function($el){
-          // after window ($el) snaps into place
-      },
-      onWindowEnter: function($el){
-          // when new window ($el) enters viewport
-      }
-  })
-
   // $('img').adipoli({
   //   'startEffect' : 'grayscale',
   //   'hoverEffect' : 'popout'
   // });
-  
+
   $('#project-phlickr').mouseover(function () {
     $('#phlickr-details').show();
   })
